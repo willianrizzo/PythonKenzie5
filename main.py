a08353d42b0c1ab38310e22bfab394c490bafecc
willianrizzo <willianrizzoratao10@gmail.com>
2023-09-15 17:54:17 -0300

    kenziePython5

---
+++ main.py
@@ -0,0 +1,16 @@
+from residencias.residencia import Residencia, Apartamento
+
+def main():
+    residencia = Residencia(2, 7.2, "rua dois", 40000, "10|07|2022")
+    # print(residencia)
+    # print("-"*72)
+    # print(residencia.monstrar_detalhes())
+
+    apt = Apartamento(4, 50.12,"Rua Cinco", 8200.6, "11|03|1999", 7, 3)
+    print(apt)
+    print("-"*72)
+    print(apt.monstrar_detalhes())
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ residencias/residencia.py
@@ -0,0 +1,40 @@
+class Residencia:
+    def __init__(self, 
+        num_quartos: int, 
+        area: float, 
+        endereco: str, 
+        preco: float, 
+        alugado_em: str
+    ) -> None:
+        self.num_quartos = num_quartos
+        self.area = area
+        self.endereco = endereco
+        self.preco = preco
+        self.alugao_em = alugado_em
+
+    def __repr__(self) -> str:
+        return f"<Residencia: {self.endereco} - {self.preco}>"
+
+    def monstrar_detalhes(self) -> str:
+        return(
+            f"<Localiado em: {self.endereco}\n"
+            f"QTD quartos {self.num_quartos}\n"
+            f"Valor total: {self.preco}>"
+        )
+        # return f"<Localiado em: {self.endereco}\n QTD quartos {self.num_quartos}\n Valor total: {self.preco}"
+
+
+class Apartamento(Residencia):
+    def __init__(self, 
+        num_quartos: int, 
+        area: float, 
+        endereco: str, 
+        preco: float, 
+        alugado_em: str,
+        andar: int, 
+        num_elevadores: int
+
+        ) -> None:
+        super().__init__(num_quartos, area, endereco, preco, alugado_em)
+        self.andar = andar
+        self.num_elevadores = num_elevadores
